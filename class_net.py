b02fd7e2144e9cc2892fb4c614fb129577b3e34a
lowkey <247730002@qq.com>
2023-04-19 14:46:56 +0800

    save and load

--- class_net.py
+++ class_net.py
@@ -3,7 +3,37 @@ import matplotlib
 matplotlib.use('TkAgg',force=True)
 from matplotlib import pyplot as plt
 import numpy as np
-from model.Net import Net as SampleNet
+from model.Net import Net as SampleNet    
+
+def train(net, count, draw_step, loss_f, opt):
+    plt.figure(draw_step)
+    for i in range(count):
+        out = net(x)
+        loss = loss_f(out, y)    
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+        if i % draw_step == 0:
+            plt.cla()
+            # plot and show learning process
+            # 从输出数据集中按照列取最大值，取完是一个100*1的张量
+            out_max = torch.max(out, 1)
+            # print(out_max)
+            # print(out_max.values)
+            # print(out_max.indices)
+            # 这里要取的是最大值对应的索引，通过序号1或者indices都可以
+            # 结果要么是0，要么是1
+            prediction = out_max[1]
+            pred_y = prediction.data.numpy()
+            target_y = y.data.numpy()
+            plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=prediction, s=100, lw=0, cmap='RdYlGn')
+            # 转化成numpy是为了方便计算
+            preSum = (pred_y == target_y).astype(int)
+            # 如果全部预测准确，应该和target_y.size一样大
+            sum = preSum.sum()
+            accuracy = float(sum) / float(target_y.size)
+            plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
+            plt.pause(0.1)
 
 n_data = torch.ones(100, 2)
 # 标准差越大曲线越平缓，标准差越小，曲线越陡峭
@@ -22,45 +52,35 @@ net_quick = torch.nn.Sequential(
     torch.nn.Linear(10, 2)
 )
 # 由于输出数据是100*2（100行2列），所以输入feature需要为2，一次取一行
-# net = SampleNet(2,10,2)
-net = net_quick
+net = SampleNet(2,10,2)
 
-opt = torch.optim.SGD(net.parameters(), lr = 0.002)
-loss_f = torch.nn.CrossEntropyLoss()
+opt_function = torch.optim.SGD(net.parameters(), lr = 0.002)
+loss_function = torch.nn.CrossEntropyLoss()
+
 
 #开启交互模式，如果开启了plt.figure()才能显示窗口
 plt.ion()
+sampleNetName = "sampleNet.pkl"
+sampleNetParameters = "sampleNetParameters.pkl"
 
-for i in range(200):
-    out = net(x)
-    loss = loss_f(out, y)
-    
-    opt.zero_grad()
-    loss.backward()
-    opt.step()
-    
-    if i % 2 == 0:
-        # plot and show learning process
-        plt.cla()
-        # 从输出数据集中按照列取最大值，取完是一个100*1的张量
-        out_max = torch.max(out, 1)
-        # print(out_max)
-        # print(out_max.values)
-        # print(out_max.indices)
-        # 这里要取的是最大值对应的索引，通过序号1或者indices都可以
-        # 结果要么是0，要么是1
-        prediction = out_max[1]
-        pred_y = prediction.data.numpy()
-        target_y = y.data.numpy()
-        plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=prediction, s=100, lw=0, cmap='RdYlGn')
-        # 转化成numpy是为了方便计算
-        preSum = (pred_y == target_y).astype(int)
-        # 如果全部预测准确，应该和target_y.size一样大
-        sum = preSum.sum()
-        accuracy = float(sum) / float(target_y.size)
-        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
-        plt.pause(0.1)
-        
+train(net, 200, 2,loss_function,opt_function)
+ 
+print("doing save!")
+torch.save(net, sampleNetName) 
+torch.save(net.state_dict(), sampleNetParameters)
+print("save done!")
+
+print("loading to net1")
+net1 = torch.load(sampleNetName)
+print("train net1")
+train(net1, 300, 3, loss_function, opt_function)
+
+print("loading to net2")
+net2 = SampleNet(2, 10, 2)
+net2.load_state_dict(torch.load(sampleNetParameters))
+print("train net2")
+train(net2, 400, 4, loss_function, opt_function)
+
 plt.ioff()
 # 没有这一步也会显示图像，这一行代码是为了阻塞程序，防止退出
 plt.show()

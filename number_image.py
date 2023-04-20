6e7b973163aa888b4fc79a7df1e4bc82e4bbdbb2
lowkey <247730002@qq.com>
2023-04-20 17:56:43 +0800

    test 数据通过dataloader加载

--- number_image.py
+++ number_image.py
@@ -24,15 +24,9 @@ train_data = torchvision.datasets.MNIST(DATASET_DIR,
 print(train_data.data.size())
 print(train_data.targets.size())
 
-# print(train_data.data[0])
-test_data = torchvision.datasets.MNIST(DATASET_DIR,train=False)
-test_data_image = test_data.test_data[:TEST_DATA_BANTCH].type(torch.FloatTensor)/255
-# print(test_data_image[0])
-test_data_image = torch.unsqueeze(test_data_image, 1)
-if USE_RNN:
-    test_data_image = test_data_image.view(-1, 28, 28)
+test_data = torchvision.datasets.MNIST(DATASET_DIR,train=False,transform=torchvision.transforms.ToTensor())
+test_dataloader = Data.DataLoader(test_data, batch_size=TEST_DATA_BANTCH, shuffle=True)
 test_data_label = test_data.targets[:TEST_DATA_BANTCH]
-
 
 dataloader = Data.DataLoader(train_data,BANTCH_SIZE,shuffle=True)
 
@@ -69,34 +63,38 @@ for i in range(EPOCH):
         opt.step()
         
         if step % BANTCH_SIZE == 0:
-            test_out = net(test_data_image)
-            # 取最大值的序号
-            pre_out = torch.max(test_out, 1)[1]
-            value = sum(pre_out == test_data_label) / test_data_label.size(0)
-            print('准确率：', value)
-            
-            x = torch.arange(0, TEST_DATA_BANTCH, 1)
-            # 数据增加一个维度
-            # pre_out = torch.unsqueeze(pre_out, 1)
-            # 增加一个维度，纵坐标0-100递增，需注意cat的两个形状需要一样。可以通过size()==size()或者shape==shape进行判断
-            # pre_out = torch.cat((torch.unsqueeze(torch.arange(0, 100, 1), dim=1), pre_out), dim=1)
-            # lable_compare = torch.unsqueeze(test_data_label, 1)
-            # lable_compare = torch.cat((torch.unsqueeze(torch.arange(0, 100, 1),dim=1), lable_compare), dim=1)
-            # cla同clear
-            ax.cla()
-            # cla以后需要重新设置，刻度，label等
-            ax.set_ylim(0,15)
-            ax.set_ylabel('number')
-            ax.set_xlabel('bantch')
-            # x坐标可以省略
-            ax.plot(x, pre_out.numpy(),'o-r', label='yuce')
-            ax.plot(x, test_data_label.numpy(),'.--b', label='jieguo')
-            # 应该拿出去避免重复计算，图省事就直接/2了
-            ylim = ax.get_ylim()
-            xlim = ax.get_xlim()
-            ax.text(xlim[1]/5, ylim[1] - ylim[1] * 0.1,'step: {}, loss: {}, accuracy rate: {:.2f}'.format(step,loss, value))
-            # legend必须放到plot下面
-            ax.legend()
-            plt.pause(0.5)
+            for _, (x_data, y_data) in enumerate(test_dataloader):
+                if USE_RNN:
+                    x_data = x_data.view(-1,28,28)
+                test_out = net(x_data)
+                # 取最大值的序号
+                pre_out = torch.max(test_out, 1)[1]
+                value = sum(pre_out == y_data) / y_data.size(0)
+                print('准确率：', value)
+                
+                x = torch.arange(0, TEST_DATA_BANTCH, 1)
+                # 数据增加一个维度
+                # pre_out = torch.unsqueeze(pre_out, 1)
+                # 增加一个维度，纵坐标0-100递增，需注意cat的两个形状需要一样。可以通过size()==size()或者shape==shape进行判断
+                # pre_out = torch.cat((torch.unsqueeze(torch.arange(0, 100, 1), dim=1), pre_out), dim=1)
+                # lable_compare = torch.unsqueeze(test_data_label, 1)
+                # lable_compare = torch.cat((torch.unsqueeze(torch.arange(0, 100, 1),dim=1), lable_compare), dim=1)
+                # cla同clear
+                ax.cla()
+                # cla以后需要重新设置，刻度，label等
+                ax.set_ylim(0,15)
+                ax.set_ylabel('number')
+                ax.set_xlabel('bantch')
+                # x坐标可以省略
+                ax.plot(x, pre_out.numpy(),'o-r', label='yuce')
+                ax.plot(x, y_data.numpy(),'.--b', label='jieguo')
+                # 应该拿出去避免重复计算，图省事就直接/2了
+                ylim = ax.get_ylim()
+                xlim = ax.get_xlim()
+                ax.text(xlim[1]/5, ylim[1] - ylim[1] * 0.1,'step: {}, loss: {}, accuracy rate: {:.2f}'.format(step,loss, value))
+                # legend必须放到plot下面
+                ax.legend()
+                plt.pause(0.5)
+                break
             
             

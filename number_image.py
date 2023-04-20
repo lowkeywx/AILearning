32300530696eb3240d6b60cc870ecb140a74334e
lowkey <247730002@qq.com>
2023-04-20 15:35:45 +0800

    对象方式绘图

--- number_image.py
+++ number_image.py
@@ -10,6 +10,7 @@ BANTCH_SIZE=50
 LR=0.2
 DOWLOAD_DATA=False
 DATASET_DIR='./mnist'
+TEST_DATA_BANTCH=100
 
 train_data = torchvision.datasets.MNIST(DATASET_DIR,
                                         train=True,
@@ -19,9 +20,9 @@ train_data = torchvision.datasets.MNIST(DATASET_DIR,
                                         )
 
 test_data = torchvision.datasets.MNIST(DATASET_DIR,train=False)
-test_data_image = test_data.test_data[:100]
+test_data_image = test_data.test_data[:TEST_DATA_BANTCH]
 test_data_image = torch.unsqueeze(test_data_image, 1).type(torch.FloatTensor)
-test_data_label = test_data.test_labels[:100]
+test_data_label = test_data.test_labels[:TEST_DATA_BANTCH]
 
 
 dataloader = Data.DataLoader(train_data,BANTCH_SIZE,shuffle=True)
@@ -39,7 +40,9 @@ net = CNN(1, 10, 28, 28)
 opt = torch.optim.Adam(net.parameters())
 loss_function = torch.nn.CrossEntropyLoss()
 # 这里需要设置一个新的figure，否则坐标系纵坐标会被压缩, (8,8)=(800,800)
-plt.figure(2,figsize=(8,8))
+figure2 = plt.figure(2,figsize=(8,8))
+ax = figure2.add_subplot(111)
+
 for i in range(EPOCH):
     for step, (bantch_x, bantch_y) in enumerate(dataloader):
         out = net(bantch_x)
@@ -51,28 +54,33 @@ for i in range(EPOCH):
         
         if step % BANTCH_SIZE == 0:
             test_out = net(test_data_image)
-            
+            # 取最大值的序号
             pre_out = torch.max(test_out, 1)[1]
             value = sum(pre_out == test_data_label) / test_data_label.size(0)
             print('准确率：', value)
             
-            x = torch.arange(0, 100, 1)
+            x = torch.arange(0, TEST_DATA_BANTCH, 1)
             # 数据增加一个维度
             # pre_out = torch.unsqueeze(pre_out, 1)
             # 增加一个维度，纵坐标0-100递增，需注意cat的两个形状需要一样。可以通过size()==size()或者shape==shape进行判断
             # pre_out = torch.cat((torch.unsqueeze(torch.arange(0, 100, 1), dim=1), pre_out), dim=1)
             # lable_compare = torch.unsqueeze(test_data_label, 1)
             # lable_compare = torch.cat((torch.unsqueeze(torch.arange(0, 100, 1),dim=1), lable_compare), dim=1)
-            plt.cla()
+            # cla同clear
+            ax.cla()
             # cla以后需要重新设置，刻度，label等
-            plt.ylim(0,15)
-            plt.ylabel('number')
-            plt.xlabel('bantch')
+            ax.set_ylim(0,15)
+            ax.set_ylabel('number')
+            ax.set_xlabel('bantch')
             # x坐标可以省略
-            plt.plot(x, pre_out.numpy(),'o-r', label='yuce')
-            plt.plot(x, test_data_label.numpy(),'.--b', label='jieguo')
+            ax.plot(x, pre_out.numpy(),'o-r', label='yuce')
+            ax.plot(x, test_data_label.numpy(),'.--b', label='jieguo')
+            # 应该拿出去避免重复计算，图省事就直接/2了
+            ylim = ax.get_ylim()
+            xlim = ax.get_xlim()
+            ax.text(xlim[1]/2, ylim[1] - ylim[1] * 0.1,'accuracy rate: {:.2f}'.format(value))
             # legend必须放到plot下面
-            plt.legend(loc='best')
-            plt.pause(0.1)
+            ax.legend()
+            plt.pause(0.5)
             
             
